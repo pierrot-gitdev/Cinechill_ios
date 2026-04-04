07809e469b22e5cfd638ffd9d384da45abcb83ed
Pierre Robert <pierre@footbar.com>
2026-04-04 18:35:47 +0200

    Initial Commit

---
+++ Cinechill_iOS/Cinechill_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cinechill_iOSApp.swift
+//  Cinechill_iOS
+//
+//  Created by Pierre ROBERT on 04/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct Cinechill_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cinechill_iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cinechill_iOS
+//
+//  Created by Pierre ROBERT on 04/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
